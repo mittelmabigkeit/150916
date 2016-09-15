70a7cd2325f1ee29837426a6785c334619defba3
mittelmabigkeit <comp422012016@mail.ru>
2016-09-15 17:38:06 +0300

    update

--- main.js
+++ main.js
@@ -12,3 +12,20 @@ console.log(sumBellForGoTo);
 
 sumBellForGoTo.abcd=12345;
 console.log(sumBellForGoTo);
+
+var truegods={
+	"azazel":999,
+	"velial":"squad",
+	"mefisto":4.20,
+	"lucifer":true,
+	"LaVey":{bible:666,church:"burn"}
+}
+console.log(truegods.LaVey.bible);
+
+
+var exampleobject=Object.create({x:5,y:"text",z:3.8,s:false,f:{one:1,two:2}})
+for (var l in exampleobject)
+	console.log(l);
+
+var obj = new Object({q:8,w:"asd",e:4.4,r:true,t:{aaa:111,two:222}})
+console.log(obj);
