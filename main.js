2a6392aa4919124ca03859a772563b1e136ecd9b
mittelmabigkeit <comp422012016@mail.ru>
2016-09-15 16:28:48 +0300

    update

--- main.js
+++ main.js
@@ -2,10 +2,13 @@ var sumBellForGoTo={
 	"atlantic":666,
 	"num":14.88,
 	"str_one":'ave satani',
-	"logic":true
+	"logic":true,
 }
 for (var a in sumBellForGoTo)
 	console.log(a);
 
 delete sumBellForGoTo["num"];
+console.log(sumBellForGoTo);
+
+sumBellForGoTo.abcd=12345;
 console.log(sumBellForGoTo);
